dd7b74151b60fc0291b93d97f2774c908769a533
Tijs-vd-B <81232322+Tijs-vd-B@users.noreply.github.com>
2021-06-01 01:38:14 +0200

    Some more styling

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Container from "./Container";
 function App() {
   return (
     <div className="App">
-      <h1>SHeLP</h1>
-      <h2>the shop help</h2>
+      <div className="header">
+        <h1>SHeLP</h1>
+        <h2>the shop help</h2>
+      </div>
       <Container />
     </div>
   );
